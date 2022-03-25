3b639a975792f5399b86feaf58bbafccfd26c88d
Syariifhdyt <Syariifhdyt@gmail.com>
2022-03-26 00:10:32 +0700

    Biodata

---
+++ application/controllers/Biodata.php
@@ -0,0 +1,11 @@
+<?php
+class Biodata extends CI_Controller
+{
+    public function index()
+    {
+        echo"<h1> Hallo, Perkenalkan...</h1>";
+        echo"Nama Saya SYARIF HIDAYAT<br>";
+        echo"Saya tinggal di Kebon Baru, Tebet.
+            Hobi saya bermain Sepak Bola."; 
+    }
+}
